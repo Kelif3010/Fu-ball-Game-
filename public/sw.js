46849cf0718348969842a4c1512459006e6d0279
Kelif3010 <elfiandken@icloud.com>
2026-06-13 08:59:36 +0200

    Cache FIFA ranking badge script in app shell

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "wm-liga-shell-v1";
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"];
+const CACHE_NAME = "wm-liga-shell-v2";
+const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg", "/fifa-ranks.js"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
