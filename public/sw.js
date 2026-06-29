209cd36ca1f5c08aaee94a98f598b2772082e5ea
Kelif3010 <elfiandken@icloud.com>
2026-06-29 10:56:56 +0200

    Bump PWA shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wm-liga-shell-v5";
+const CACHE_NAME = "wm-liga-shell-v6";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg", "/fifa-ranks.js"];
 
 self.addEventListener("install", event => {
